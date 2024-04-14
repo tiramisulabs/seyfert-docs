b2bbeba9a925ad96b5e6dacbbab610d1a71b9e98
Fabrizio Santana <74118796+FabrizioCoder@users.noreply.github.com>
2024-04-14 16:28:25 -0600

    Added Spanish translations for sidebar (#26)
    * Added Spanish translations for sidebar
    
    * Update contributors.astro

--- sidebar.items.mjs
+++ sidebar.items.mjs
@@ -1,113 +1,189 @@
 import { typeDocSidebarGroup } from './src/lib/startlight-typedoc';
 
-export default [{
-	label: 'Guide',
-	items: [
-		{
-			label: 'Introduction',
-			link: '/',
-		},
-		{
-			label: 'Getting Started',
-			collapsed: true,
-			items: [
-				{
-					label: 'Setup seyfert project',
-					link: '/guides/setup-project'
-				},
-				{
-					label: 'Start from example',
-					link: '/guides/start-from-example'
-				},
-				{
-					label: 'First Command',
-					link: '/guides/first-command'
-				},
-				{
-					label: 'Listening discord events',
-					link: '/guides/listening-events'
-				},
-				{
-					label: 'Understanding declare module',
-					link: '/guides/declare-module'
-				}
-			]
-		},
-		{
-			label: "Commands",
-			collapsed: true,
-			items: [
-				{
-					label: 'Command Options',
-					link: '/commands/options'
-				},
-				{
-					label: 'Command Decorators',
-					link: '/commands/decorators'
-				},
-				{
-					label: 'Command Middlewares',
-					link: '/commands/middlewares'
-				},
-				{
-					label: 'Sub-Zero (Sub Commands)',
-					link: '/commands/subcommands'
-				},
-				{
-					label: 'Extending Commands',
-					link: '/commands/command-class'
-				},
-				{
-					label: 'Prefix Commands',
-					link: '/commands/prefix-commands'
-				},
-			]
-		},
-		{
-			label: 'Components',
-			collapsed: true,
-			items: [
-				{
-					label: 'Introduction',
-					link: '/components/introduction'
-				},
-				{
-					label: 'Building Components',
-					link: '/components/building-components'
-				},
-				{
-					label: 'Handling Components',
-					link: '/components/handling-components'
-				},
-				{
-					label: 'Modals',
-					link: '/components/modals'
-				},
-				{
-					label: 'Collectors',
-					link: '/components/collectors'
-				}
-			]
-		},
-		{
-			label: 'Miscellaneous',
-			collapsed: true,
-			items: [
-				{
-					label: 'Sending Messages',
-					link: '/misc/sending-messages'
-				},
-				{
-					label: 'Music library',
-					link: '/misc/music'
-				},
-				{
-					label: 'API Access',
-					link: '/misc/api-access'
-				}
-			]
-		}
-	]
-},
-	typeDocSidebarGroup
-]
+export default [
+  {
+    label: 'Guide',
+	translations: {
+		es: 'Guía',
+	},
+    items: [
+      {
+        label: 'Introduction',
+        translations: {
+          es: 'Introducción',
+        },
+        link: '/',
+      },
+      {
+        label: 'Getting Started',
+        translations: {
+          es: 'Empezando',
+        },
+        collapsed: true,
+        items: [
+          {
+            label: 'Setup seyfert project',
+            translations: {
+              es: 'Configuración de Seyfert',
+            },
+            link: '/guides/setup-project',
+          },
+          {
+            label: 'Start from example',
+            translations: {
+              es: 'Empezar desde un ejemplo',
+            },
+            link: '/guides/start-from-example',
+          },
+          {
+            label: 'First Command',
+            translations: {
+              es: 'Creando tu primer comando',
+            },
+            link: '/guides/first-command',
+          },
+          {
+            label: 'Listening discord events',
+            translations: {
+              es: 'Escuchando los eventos de Discord',
+            },
+            link: '/guides/listening-events',
+          },
+          {
+            label: 'Understanding declare module',
+            translations: {
+              es: 'Entendiendo declare module',
+            },
+            link: '/guides/declare-module',
+          },
+        ],
+      },
+      {
+        label: 'Commands',
+        translations: {
+          es: 'Comandos',
+        },
+        collapsed: true,
+        items: [
+          {
+            label: 'Command Options',
+            translations: {
+              es: 'Opciones de un Comando',
+            },
+            link: '/commands/options',
+          },
+          {
+            label: 'Command Decorators',
+            translations: {
+              es: 'Decoradores para el Comando',
+            },
+            link: '/commands/decorators',
+          },
+          {
+            label: 'Command Middlewares',
+            translations: {
+              es: 'Middlewares de Comandos',
+            },
+            link: '/commands/middlewares',
+          },
+          {
+            label: 'Sub-Zero (Sub Commands)',
+            translations: {
+              es: 'Sub-Zero (Sub Comandos)',
+            },
+            link: '/commands/subcommands',
+          },
+          {
+            label: 'Extending Commands',
+            translations: {
+              es: 'Extendiendo Comandos',
+            },
+            link: '/commands/command-class',
+          },
+          {
+            label: 'Prefix Commands',
+            translations: {
+              es: 'Comandos de prefijo',
+            },
+            link: '/commands/prefix-commands',
+          },
+        ],
+      },
+      {
+        label: 'Components',
+        translations: {
+          es: 'Componentes',
+        },
+        collapsed: true,
+        items: [
+          {
+            label: 'Introduction',
+            translations: {
+              es: 'Introducción',
+            },
+            link: '/components/introduction',
+          },
+          {
+            label: 'Building Components',
+            translations: {
+              es: 'Construyendo Componentes',
+            },
+            link: '/components/building-components',
+          },
+          {
+            label: 'Handling Components',
+            translations: {
+              es: 'Manejando Componentes',
+            },
+            link: '/components/handling-components',
+          },
+          {
+            label: 'Modals',
+            translations: {
+              es: 'Modals',
+            },
+            link: '/components/modals',
+          },
+          {
+            label: 'Collectors',
+            translations: {
+              es: 'Collectors',
+            },
+            link: '/components/collectors',
+          },
+        ],
+      },
+      {
+        label: 'Miscellaneous',
+        translations: {
+          es: 'Misceláneos',
+        },
+        collapsed: true,
+        items: [
+          {
+            label: 'Sending Messages',
+            translations: {
+              es: 'Enviando Mensajes',
+            },
+            link: '/misc/sending-messages',
+          },
+          {
+            label: 'Music library',
+            translations: {
+              es: 'Librería de música',
+            },
+            link: '/misc/music',
+          },
+          {
+            label: 'API Access',
+            translations: {
+              es: 'Acceso a la API',
+            },
+            link: '/misc/api-access',
+          },
+        ],
+      },
+    ],
+  },
+  typeDocSidebarGroup,
+];
