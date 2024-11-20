37f968d0c82693d594c36a897c341f0a1bb2c549
MARCROCK22 <marcos22dev@gmail.com>
2024-11-19 21:38:52 -0400

    fix

--- src/docs.d.ts
+++
@@ -1,5 +0,0 @@
-import type { ParseClient, Client } from 'seyfert';
-
-declare module 'seyfert' {
-  interface UsingClient extends ParseClient<Client<true>> {}
-}
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference path="../node_modules/@astrojs/starlight/virtual.d.ts"/>
+/// <reference path="../node_modules/@astrojs/starlight/virtual-internal.d.ts"/>
